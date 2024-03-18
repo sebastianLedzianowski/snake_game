6b8dbc74f510e9564793952d9395cdd8297a8745
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-03-18 17:09:55 +0100

    Add position.py

---
+++ position.py
@@ -0,0 +1,15 @@
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def __eq__(self, other):
+        return isinstance(other, Position) and \
+            self.x == other.x and \
+            self.y == other.y
+
+    def __str__(self):
+        return f"({self.x}, {self.y}"
+
+    __repr__ = __str__
+    
