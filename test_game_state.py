626f82b82aea286d8a9062c8bc293345a4068c3d
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-03-24 12:57:39 +0100

    delete test_snake_dies

--- test_game_state.py
+++ test_game_state.py
@@ -204,29 +204,6 @@ class GameStateTest(unittest.TestCase):
         self.assertEqual(False, state.food in state.snake)
 
 
-    def test_snake_dies(self):
-        state = GameState(
-            snake=[
-                Position(1, 2),
-                Position(2, 2),
-                Position(3, 3),
-                Position(2, 3)
-            ],
-            direction=Direction.UP,
-            food=Position(3, 1),
-            field_size=25
-        )
-
-        state.step()
-
-        from game_state import INITIAL_SNAKE
-        self.assertEqual(INITIAL_SNAKE, state.snake)
-        self.assertFalse(state.food in state.snake)
-        from game_state import INITIAL_DIRECTION
-        self.assertEqual(INITIAL_DIRECTION, state.direction)
-        self.assertEqual(25, state.field_size)
-
-
     def test_turn(self):
         state = GameState(
             snake=[
